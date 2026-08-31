09a41ad8fb23bc2f458c07ec560721c06dc6069a
speak-empowered <nathan.mitchell6102@gmail.com>
2026-08-31 17:06:46 -0500

    Add files via upload

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 // Paste the Square checkout URL between the quotes below before launch.
-const LEADERSHIP_LAB_CHECKOUT_URL = "";
+const LEADERSHIP_LAB_CHECKOUT_URL = "https://checkout.square.site/merchant/8KVR1X00JS91M/checkout/3KGZVV7RM6GLI4VBUWOD7FDX";
 
 const checkoutButton = document.getElementById("checkout-button");
 const checkoutNote = document.getElementById("checkout-note");
